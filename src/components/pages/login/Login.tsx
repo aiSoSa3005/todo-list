3435394d096c4799022601e2026afc109ba4ec01
Prosper <you@example.com>
2025-02-06 13:58:36 +0100

    enhance login component

--- src/components/pages/login/Login.tsx
+++ src/components/pages/login/Login.tsx
@@ -10,20 +10,21 @@ const [error,seterror] = useState('')
   return (
     <>
 
-    <form action="">
-    <h1>Login</h1>
-        <div className="form-group">
-            <label htmlFor="email">Email</label>
-            <input type="email" value={email} id='email'/>
-        </div>
-        
-        <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input type="password" value={password} id='password'/>
-        </div>
-
-        <button type='submit'>Log in</button>
-    </form>
+    <div className="form-container">
+      <form action="">
+      <h1>Login</h1>
+          <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input type="email" value={email} id='email' placeholder='Enter your email'/>
+          </div>
+      
+          <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input type="password" value={password} id='password' placeholder='Enter your password'/>
+          </div>
+          <button type='submit'>Log in</button>
+      </form>
+    </div>
 
 
     </>
