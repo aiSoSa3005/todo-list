8933a3639d4bd41520275c0926c426b1d82f51cd
Prosper <you@example.com>
2025-02-06 14:33:48 +0100

    draft icon eye

--- src/components/pages/login/Login.tsx
+++ src/components/pages/login/Login.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { FaRegEye } from "react-icons/fa";
+import { FaRegEyeSlash } from "react-icons/fa6";
 import './Login.css'
 
 const Login = () => {
@@ -6,6 +8,12 @@ const Login = () => {
 const [email,setEmail] = useState('')
 const [password,setPassword] = useState('')
 const [error,seterror] = useState('')
+const [isVisible,setIsVisibile] = useState(false)
+
+const handleVisibility = ()=>{
+    setIsVisibile(!isVisible)
+}
+
 
   return (
     <>
@@ -15,12 +23,12 @@ const [error,seterror] = useState('')
       <h1>Login</h1>
           <div className="form-group">
               <label htmlFor="email">Email</label>
-              <input type="email" value={email} id='email' placeholder='Enter your email'/>
+              <div className="input-container"><input onChange={(e=> setEmail(e.target.value))} type="email" value={email} id='email' placeholder='Enter your email'/></div>
           </div>
       
           <div className="form-group">
               <label htmlFor="password">Password</label>
-              <input type="password" value={password} id='password' placeholder='Enter your password'/>
+              <div className="input-container"><input onChange={(e)=> setPassword(e.target.value)} type={ isVisible ? 'text' : 'password'} value={password} id='password' placeholder='Enter your password'/><div className=''  onClick={handleVisibility}>{ isVisible ? <FaRegEye /> : <FaRegEyeSlash />}</div ></div>
           </div>
           <button type='submit'>Log in</button>
       </form>
